dd289103e6688219a350eb9335323a1d4fc1a15d
Faizee.Bano <faizee.bano@infobeans.com>
2021-10-21 19:17:22 +0530

    added style for scrollbar

--- src/components/chatinput/ChatInput.jsx
+++ src/components/chatinput/ChatInput.jsx
@@ -4,12 +4,14 @@ import "./Chatinput.css";
 const ChatInput = () => {
 
   return (
+    <div class="p-2">
     <div class="input">
       <form class="row">
         <div class="input-group mb-1 inner-addon right-addon">
           <textarea class="form-control input-box " id="exampleFormControlTextarea3" placeholder="Type your message here"></textarea>
         </div>
       </form>
+    </div>
     </div>
   );
 };
